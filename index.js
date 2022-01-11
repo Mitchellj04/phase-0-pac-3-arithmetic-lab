2eb28f396b01541eff29555cec0dd9e473e4b739
JustinMitchell <mitchelljm04@gmail.com>
2022-01-10 20:54:10 -0500

    Completed Assignment

---
+++ index.js
@@ -0,0 +1,38 @@
+
+function add(){
+    return (number, number2)
+}
+function subtract(){
+    return (number, number2)
+}
+function multiply(){
+    return (number, number2)
+}
+function divide(){
+    return (number, number2)
+}
+function add(a,b){
+    return a+b;
+}
+function subtract(a, b){
+    return a-b;
+}
+function multiply(a, b){
+    return a*b;
+}
+function divide(a, b){
+    return a/b;
+}
+function increment(n){
+    return ++n;
+}
+function decrement(n){
+    return --n;
+}
+
+function makeInt(n){
+    return parseInt(n, 10);
+}
+function preserveDecimal(n){
+    return parseFloat(n);
+}
